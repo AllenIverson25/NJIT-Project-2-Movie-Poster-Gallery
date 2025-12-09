fa397c94d9cf7070e58c87ea6095a5d5c5f66e1d
AllenIverson25 <526jfriedman@frhsd.com>
2025-12-09 19:19:36 +0000

    Finalized website

--- site.js
+++ site.js
@@ -1,46 +1,24 @@
-/* SITE.JS: THIS FILE CONTAINS THE METHODS/FUNCTIONS AND VARIABLES CONTROLLING YOUR SITE
-//
-*/
-
-/* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
-//
-//    They are in the following format:
-//    title (String): the name of the movie
-//    iscore (Number): the IMDB score
-//    rating (String): the movie's MPAA rating
-//    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
-//    country (String): the country of production
-//    posters (Array): an array of String values with the URL to movie posters (in your img/ directory)
-//    imdb (String): the URL to the corresponding IMDB website
-//    website (String): the URL to the corresponding official website
-//    likes (Number): a fictitious number of user likes
-//    dislikes (Number): a fictitious number of user dislikes
-//    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
-//
-// FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
-*/
-
+/* SITE.JS: THIS FILE CONTAINS THE METHODS/FUNCTIONS AND VARIABLES CONTROLLING YOUR SITE */
 
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
       created () {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
+      mounted() {
+            // Initialize 3D Parallax Tilt Effect
+            this.init3DParallax()
+      },
       data() {
         return {
-            // This holds your movies.json data.
             movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
             title: "IMDB + Joseph Friedman's Top 8 Movies",
             owner: "Joseph Friedman",
             github: "https://github.com/AllenIverson25/NJIT-Project-2-Movie-Poster-Gallery"
       }
     },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
             makeTextDate(dateArray) {
                   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
                   return months[dateArray[1] - 1] + " " + dateArray[2] + ", " + dateArray[0];
@@ -62,6 +40,36 @@ const vue_app = Vue.createApp({
                   let hours = Math.floor(minutes / 60);
                   let mins = minutes % 60;
                   return hours + "h " + mins + "m";
+            },
+            
+            // 3D PARALLAX TILT EFFECT - EXTRA SUBTLE VERSION
+            init3DParallax() {
+                  // Wait a bit for Vue to render the cards
+                  setTimeout(() => {
+                        const cards = document.querySelectorAll('.thumbnail');
+                        
+                        cards.forEach(card => {
+                              card.addEventListener('mousemove', (e) => {
+                                    const rect = card.getBoundingClientRect();
+                                    const x = e.clientX - rect.left;
+                                    const y = e.clientY - rect.top;
+                                    
+                                    const centerX = rect.width / 2;
+                                    const centerY = rect.height / 2;
+                                    
+                                    // Reduced rotation from /25 to /40 for even more subtlety
+                                    const rotateX = (y - centerY) / 40;
+                                    const rotateY = (centerX - x) / 40;
+                                    
+                                    // Reduced scale from 1.02 to 1.01
+                                    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.01, 1.01, 1.01)`;
+                              });
+                              
+                              card.addEventListener('mouseleave', () => {
+                                    card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)';
+                              });
+                        });
+                  }, 500);
             }
       }
 })
